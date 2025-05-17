dc320f735d98c72c18f06da94b8a884eeaf5bde1
Aravinth <aravinth2721@gmail.com>
2025-05-17 15:39:02 +0530

    Navbar section completed in web - 17 May 2025

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ResponsiveService } from '../../services/responsive.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, AsyncPipe],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  title = 'Portfolio';
+  navData!: String[];
+  activeLink!: String;
+  public responsive = inject(ResponsiveService);
+  constructor() {
+    this.navData = ['Home', 'About', 'Projects', 'Education', 'Contact'];
+    this.activeLink = 'Home';
+    location.hash = '#home';
+  }
+  ngOnInit(): void {}
 
+  setActive(link: String) {
+    this.activeLink = link;
+  }
 }
